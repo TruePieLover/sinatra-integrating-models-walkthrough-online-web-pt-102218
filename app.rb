6ac861a672703fd419a1fb25f85e86ec0094aeb6
Brandon Teich <brandonteich123@gmail.com>
2019-04-03 22:43:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,16 +1,13 @@
 require_relative 'config/environment'
-require_relative 'models/textanalyzer.rb'
+require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
-get '/' do
-  erb :index
-end
-
-post '/results' do
-  @analyzed_text = TextAnalyzer.new(params[:user_text])
-  erb :results
-end
-
-
+  post '/' do
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    erb :results
+  end
 end
